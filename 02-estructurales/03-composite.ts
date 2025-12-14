a67c37fdcdedfcd5a51bd2e57600cc7f349bc005
David Vivanco <davidv2108@gmail.com>
2025-12-14 03:51:30 +0100

    implementación del patrón estructural Composite.

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,68 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+  showDetails(indent?: string): void;
+}
+
+class FileExample implements FileSystemComponent {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  showDetails(indent?: string): void {
+    console.log(`${indent || ''}- File: ${this.name}`);
+  }
+}
+
+class Directory implements FileSystemComponent {
+    private name: string;
+    private contents: FileSystemComponent[] = [];
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    showDetails(indent?: string): void {
+        console.log(`${indent || ''}+ Directory: ${this.name}`);
+        this.contents.forEach(component => component.showDetails((indent || '') + '  '));
+    }
+
+    add(component: FileSystemComponent): void {
+        this.contents.push(component);
+    }
+}
+
+function main(){
+
+    const file1 = new FileExample('file1.txt');
+    const file2 = new FileExample('file2.txt');
+    const file3 = new FileExample('file3.txt');
+    const file4 = new FileExample('file4.txt');
+
+    const folder1 = new Directory('folder1');
+    const folder5 = new Directory('folder5');
+    folder1.add(file1);
+    folder1.add(file2);
+
+    const folder2 = new Directory('folder2');
+    folder2.add(file3);
+
+    const folder3 = new Directory('folder3');
+    folder3.add(file4);
+
+    folder2.add(folder3);
+    folder2.add(folder5);
+
+    const rootFolder = new Directory('rootFolder');
+    rootFolder.add(folder1);
+    rootFolder.add(folder2);
+
+    rootFolder.showDetails();
+
+}
+
+main();
