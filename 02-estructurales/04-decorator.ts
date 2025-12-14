def4dc80af12660955dcec0ed3ce9683b4d3bd94
David Vivanco <davidv2108@gmail.com>
2025-12-14 04:26:30 +0100

    implementación de patrón estructural Decorator.

--- 02-estructurales/04-decorator.ts
+++ 02-estructurales/04-decorator.ts
@@ -11,3 +11,69 @@
  *
  * https://refactoring.guru/es/design-patterns/decorator
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface INotification {
+    send(message: string): void;
+}
+
+class BasicNotification implements INotification {
+    send(message: string): void {
+        console.log(`%cEnviando notificación básica: %c${message}`, COLORS.blue, COLORS.white);
+    }
+}
+
+//Clase decoradora
+abstract class NotificationDecorator implements INotification {
+
+    protected notification: INotification;
+
+    constructor(notification: INotification) {
+        this.notification = notification;
+    }
+
+    send(message: string): void {
+        this.notification.send(message);
+    }
+
+}
+
+class EmailDecorator extends NotificationDecorator {
+
+    private sendEmail(message: string): void {
+        console.log(`%cEnviando notificación por correo electrónico: %c${message}`, COLORS.green, COLORS.white);
+    }
+
+    override send(message: string): void {
+        super.send(message);
+        this.sendEmail(message);
+    }
+
+}
+
+class SMSDecorator extends NotificationDecorator {
+
+    private sendSMS(message: string): void {
+        console.log(`%cEnviando notificación por SMS: %c${message}`, COLORS.red, COLORS.white);
+    }
+
+    override send(message: string): void {
+        super.send(message);
+        this.sendSMS(message);
+    }
+
+}
+
+function main(){
+
+    let notification: INotification = new BasicNotification();
+
+    notification = new EmailDecorator(notification);
+    notification = new SMSDecorator(notification);
+
+    notification.send("Alerta de sistema");
+
+}
+
+main();
